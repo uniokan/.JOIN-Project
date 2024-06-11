71c5ce427e586514bb34e2a0e9e3c1e14fbef511
Okan Özel <okan.ozel@hotmail.de>
2024-06-12 01:09:25 +0200

    jsdoc by addtask.js added

--- js/addTask.js
+++ js/addTask.js
@@ -19,12 +19,18 @@ function clearTask() {
 }
 
 
+/**
+ * This function activates the medium button with background color from the start
+ */
 function activateMediumBtn() {
     let mediumBtn = document.getElementById('medium-btn');
     mediumBtn.classList.add('medium');
 }
 
 
+/**
+ * This function clears the background color of the button when another button is selected 
+ * */
 function clearSelectedPrio() {
     if (currentBtn) {
         currentBtn.classList.remove(`${currentBtn.getAttribute('data-color')}`);
@@ -52,6 +58,9 @@ function changeColorPrioBtn(btn, color) {
 }
 
 
+/**
+ * This function gets all input from the user and then saves it in SafeTaskDetails
+ */
 function getDataFromTask() {
     let email = 'okan.ozel@hotmail.de'
     let title = document.getElementById('task-title').value;
@@ -69,6 +78,9 @@ function getDataFromTask() {
 }
 
 
+/**
+ * This function fetches all created subtasks
+ */
 function getSubtasks(){
     let subtasks = document.querySelectorAll('.new-subtask-added');
 
@@ -78,6 +90,16 @@ function getSubtasks(){
 }
 
 
+/**
+ * This function saves all inputs in json
+ * 
+ * @param {string} title - contains the title from the input field
+ * @param {string} description - contains the description from the input field
+ * @param {string} date - contains the date from the input field
+ * @param {string} prio - contains the prio from the input field
+ * @param {string} subtask - contains the subtasks from JSON
+ * @returns 
+ */
 function safeTaskDetails(title, description, date, prio, subtask) {
     return {
         'title': title,
@@ -88,7 +110,12 @@ function safeTaskDetails(title, description, date, prio, subtask) {
     }
 }
 
-
+/**
+ * This function pushes the saved data in Json to database
+ * 
+ * @param {string} emailKey - the logged in email address
+ * @param {JSON} taskDetails - contains all data in JSON format
+ */
 async function pushTaskToDatabase(emailKey, taskDetails) {
     try {
         await fetch(BASE_URL + "users/" + emailKey + ".json", {
@@ -103,7 +130,9 @@ async function pushTaskToDatabase(emailKey, taskDetails) {
     }
 }
 
-
+/**
+ * This function opens the dropdown menu from assigned to and at the same time checks whether body was clicked
+ */
 function dropDownAssigendTo() {
     let assignedTo = document.getElementById('assigned-to');
     assignedTo.classList.toggle('d-none');
@@ -113,6 +142,9 @@ function dropDownAssigendTo() {
 }
 
 
+/**
+ * This function opens the dropdown menu from category and at the same time checks whether body was clicked
+ */
 function openDropDownCategory() {
     let category = document.getElementById('category');
     category.classList.toggle('d-none');
@@ -122,11 +154,22 @@ function openDropDownCategory() {
 }
 
 
+/**
+ * This function closes the drop down menu
+ * 
+ * @param {string} dropDownContent - shows the contents contained in the dropdown menu
+ */
 function closeDropDown(dropDownContent) {
     dropDownContent.classList.add('d-none');
 }
 
 
+/**
+ * This function closes the drop down menu when body is clicked
+ * 
+ * @param {string} dropDownContent - shows the contents contained in the dropdown menu
+ * @param {string} dropDownContainer - the variable is the dropdown container
+ */
 function closeDropDownWithBody(dropDownContent, dropDownContainer) {
     document.body.addEventListener('click', (event) => {
 
@@ -137,11 +180,22 @@ function closeDropDownWithBody(dropDownContent, dropDownContainer) {
 }
 
 
+/**
+ * This function checks whether clicks were made outside of dropdown menus
+ * 
+ * @param {event} event - this event is the mouse click
+ * @param {*} dropDownContent - shows the contents contained in the dropdown menu
+ * @param {*} dropDownContainer - the variable is the dropdown container
+ * @returns - true or false
+ */
 function targetOutsideOfContainer(event, dropDownContent, dropDownContainer) {
     return !dropDownContent.classList.contains('d-none') && !dropDownContent.contains(event.target) && !dropDownContainer.contains(event.target)
 }
 
 
+/**
+ * This function adds the subtask in the form of li element
+ */
 function addSubtask() {
     let input = document.getElementById('task-subtask');
     let newSubtask = document.getElementById('new-subtask');
@@ -153,11 +207,17 @@ function addSubtask() {
     }
 
     else {
-        alert('Mehr als 5 Subtasks nicht möglich und die Länge der Subtask muss zwischen 3-10 Zeichen sein!')
+        alert('Mehr als 5 Subtasks nicht möglich und die Länge der Subtask muss zwischen 3-10 Zeichen liegen!')
     }
 }
 
 
+/**
+ * This function validates the subtask
+ * 
+ * @param {string} input - this is the input from the user in the subtask field
+ * @returns - true or false
+ */
 function subtaskValidation(input) {
     return subtaskCounter <= 4 && input.value.length >= 3 && input.value.length <= 15;
 }
