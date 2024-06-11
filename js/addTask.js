201e266d80315f9a07afa187ce75256cc5f88249
Okan Özel <okan.ozel@hotmail.de>
2024-06-11 18:11:09 +0200

    prio btn function added

--- js/addTask.js
+++ js/addTask.js
@@ -9,9 +9,22 @@ function clearTask() {
     getValues.forEach(input => {
         input.value = ''
     });
+
+    activateMediumBtn();
+    clearSelectedPrio();
+  
 }
 
+function activateMediumBtn() {
+    let mediumBtn = document.getElementById('medium-btn');
+    mediumBtn.classList.add('medium');
+}
 
+function clearSelectedPrio(){
+    if (currentBtn) {
+        currentBtn.classList.remove(`${currentBtn.getAttribute('data-color')}`);
+    }
+}
 
 /**
  * The color is assigned to the button when clicked
@@ -21,12 +34,26 @@ function clearTask() {
  */
 function changeColorPrioBtn(btn, color) {
 
-    if (currentBtn) {
-        currentBtn.classList.remove(`${currentBtn.getAttribute('data-color')}`);
-    }
+    let mediumBtn = document.getElementById('medium-btn');
+    mediumBtn.classList.remove('medium')
 
+    clearSelectedPrio();
+    
     btn.classList.add(color);
     btn.setAttribute('data-color', color);
 
     currentBtn = btn;
+}
+
+function getDataFromTask() {
+    let email = 'okan.ozel@hotmail.de'
+    let title = document.getElementById('task-title');
+    let description = document.getElementById('task-description');
+    // let assignedTo = document.getElementById('task-assignedTo');
+    let date = document.getElementById('task-date');
+    let prio = currentBtn.getAttribute('data-color');
+    // let category = document.getElementById('task-category');
+    let subtask = document.getElementById('task-subtask');
+
+    console.log(email, title.value, description.value, date.value, subtask.value, prio);
 }
