f4787e4521cb98c7b4c9618ee550a29763ffcdb9
Okan Özel <okan.ozel@hotmail.de>
2024-06-14 21:10:31 +0200

    add contact popup + close added

--- js/contacts.js
+++ js/contacts.js
@@ -107,6 +107,7 @@ async function getKeyFromUser(i) {
     return key;
 }
 
+
 function showAddContactPopUp(){
     let backgroundDim = document.getElementById('background-dim');
     let addTaskPopUp = document.getElementById('add-task-pop-up');
@@ -114,10 +115,11 @@ function showAddContactPopUp(){
     backgroundDim.classList.add('background-dim');
     addTaskPopUp.classList.remove('pop-up-hidden');
     addTaskPopUp.classList.add('pop-up-100vh');
+    closePopUpOutsideContainer();
 }
 
-function closePOpUp(){
-    console.log('yes');
+
+function closePopUp(){
     let backgroundDim = document.getElementById('background-dim');
     let addTaskPopUp = document.getElementById('add-task-pop-up');
 
@@ -125,4 +127,26 @@ function closePOpUp(){
     addTaskPopUp.classList.remove('pop-up-100vh');
     addTaskPopUp.classList.add('pop-up-hidden');
 
+}
+
+
+function closePopUpOutsideContainer() {
+    let backgroundDim = document.getElementById('background-dim');
+    let popUp = document.getElementById('add-task-pop-up');
+
+    backgroundDim.addEventListener('click', event => {
+        if (!popUp.contains(event.target)) {
+            closePopUp();
+        }
+    });
+
+    backgroundDim.addEventListener('mousemove', event => {
+        if (popUp.contains(event.target)) {
+            backgroundDim.style.cursor='unset';
+        }
+
+        else{
+            backgroundDim.style.cursor='pointer';
+        }
+    });
 }
