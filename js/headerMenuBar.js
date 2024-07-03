c9f91ac42442ea5fb3cf57c54e9c33a59fbf70e5
FabianFaber <fabian-faber@hotmail.de>
2024-07-03 13:34:52 +0200

    fixes

--- js/headerMenuBar.js
+++ js/headerMenuBar.js
@@ -225,3 +225,25 @@ function getUsersInitials(name) {
 function previousPage() {
     window.history.back();
 }
+
+
+function openSite(link, id) {
+    // Setzen der Markierung für die Hintergrundfarbe nach dem Laden der neuen Seite
+    localStorage.setItem('changeBgColor', id);
+    
+    // Seite laden
+    window.location.href = link;
+}
+
+// Auf der nächsten Seite prüfen und Hintergrundfarbe ändern
+window.onload = function() {
+    let idToChangeBg = localStorage.getItem('changeBgColor');
+    if (idToChangeBg) {
+        let anchor = document.getElementById(idToChangeBg);
+        if (anchor) {
+            anchor.style.backgroundColor = "orange";
+        }
+        // Markierung nach der Verwendung löschen
+        localStorage.removeItem('changeBgColor');
+    }
+};
