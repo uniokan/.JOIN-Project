d83072057478eeb0353fa800db7a157976641390
FabianFaber <fabian-faber@hotmail.de>
2024-07-03 16:48:30 +0200

    fixes

--- js/hideMenuBar.js
+++ js/hideMenuBar.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let hideElements = localStorage.getItem('hideElements');
+    const currentURL = window.location.href;
+    const savedURL = localStorage.getItem('savedURL');
+    const hideElements = localStorage.getItem('hideElements');
+
     if (hideElements === 'true') {
         let sidebar = document.getElementById('sidebar');
         let mainContent = document.getElementById('main-content');
         let dropdown = document.getElementById('dropdown-toggle');
         let content = document.getElementById('contentPolicys');
+        let body = document.getElementById('body')
 
         if (sidebar) sidebar.style.display = 'none';
         if (mainContent) mainContent.style.display = 'none';
@@ -13,8 +17,22 @@ document.addEventListener('DOMContentLoaded', function () {
         if (content) {
             content.style.margin = '0';
             content.style.margin = '30px';
+            body.style.display = 'flex'
+            body.style.flexDirection = 'column'
+            body.style.alignItems = 'center'
+            body.style.width = '100%'
         }
+    }
 
+    localStorage.setItem('savedURL', currentURL);
+});
+
+window.addEventListener('beforeunload', function () {
+    const currentURL = window.location.href;
+    const savedURL = localStorage.getItem('savedURL');
+
+    if (currentURL !== savedURL) {
         localStorage.removeItem('hideElements');
+        localStorage.removeItem('savedURL');
     }
 });
